bb05e77a92245be5a8cba19f4488f4751d7ec768
sangyomi <kalvg7240@gmail.com>
2023-07-07 17:58:00 +0900

    11

--- 02. Data structure/011.cpp
+++ 02. Data structure/011.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <deque>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -9,9 +10,55 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N;
+    int N; 
+    cin >> N;
 
-    cout << "GSDF" << endl;
-     
+    vector<int> Arr_Num;
+    vector<char> Arr_Char;
+
+    for(int i = 0 ; i < N ; i++)
+    {
+        int T;
+        cin >> T;
+        Arr_Num.push_back(T);
+    }
+
+    stack<int> A;
+
+    int count = 1;
+    int idx = 0;
+    bool TF = true;
+
+    for(int i = 0; i < N ; i++)
+    {
+        while(Arr_Num[idx] >= count)
+        {
+        A.push(count);
+        Arr_Char.push_back('+');
+        count++;
+        }
+
+        int T = A.pop();
+        
+        if (Arr_Num[idx] != T)
+        {
+            TF = false;
+        }
+
+        Arr_Char.push_back('-');
+        idx++;
+    }
     
+    if(TF)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        for(char a : Arr_Char)
+        {
+            cout << a << " ";
+        }
+    }
+
 }
